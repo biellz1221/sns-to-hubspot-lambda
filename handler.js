0bebdebf7085354965452185c04346cbc502e9e1
Gabriel Baptista <biellz1221@gmail.com>
2021-03-11 14:31:23 -0300

    tratamento de erros

--- handler.js
+++ handler.js
@@ -5,12 +5,13 @@ module.exports.hubspot = async (event) => {
 	const snsMessage = JSON.parse(event["Records"][0].Sns.Message);
 
 	console.log("EVENTO", JSON.stringify(event, null, 2));
-	console.info("SNS Message", snsMessage.complaint);
+	console.info("SNS Message", snsMessage);
 
 	const type = snsMessage.notificationType;
-	//if (type !== "Bounce") return { statusCode: 200 };
+
 	let ticketContent = "";
 	let ticketSubject = "";
+
 	if (type === "Bounce") {
 		ticketContent = `Motivo: ${snsMessage.bounce.bounceSubType} - ${snsMessage.bounce.bouncedRecipients[0].diagnosticCode}`;
 		ticketSubject = `SNS: ${type} - ${snsMessage.mail.destination[0]}`;
@@ -20,34 +21,53 @@ module.exports.hubspot = async (event) => {
 		ticketSubject = `SNS: ${type} - ${snsMessage.mail.destination[0]}`;
 	}
 
-	// get tickets
-	const response = await axios.get(`https://api.hubapi.com/crm/v3/objects/tickets/?archived=false&hapikey=${process.env.HUBSPOT_API_KEY}`);
-	const tickets = response.data;
-	const ticketExists = tickets.results.find((ticket) => ticket.properties.subject === ticketSubject);
+	try {
+		// get tickets
+		const response = await axios.get(`https://api.hubapi.com/crm/v3/objects/tickets/?archived=false&hapikey=${process.env.HUBSPOT_API_KEY}`);
+		const tickets = response.data;
 
-	if (ticketExists !== undefined) return { statusCode: 200, body: "Já existe um ticket para este e-mail" };
+		// check if ticket already exists for the provided email
+		const ticketExists = tickets.results.find((ticket) => ticket.properties.subject === ticketSubject);
 
-	const data = JSON.stringify({
-		"properties": {
-			"content": ticketContent,
-			"hs_pipeline": "0",
-			"hs_pipeline_stage": "1",
-			"hs_ticket_priority": "MEDIUM",
-			"hubspot_owner_id": process.env.TICKET_OWNER_ID.toString(),
-			"subject": ticketSubject,
-		},
-	});
+		if (ticketExists !== undefined) return { statusCode: 400, body: "There's already a Ticket for this email." };
 
-	// create ticket
-	// const postResponse =
-	await axios.post(`https://api.hubapi.com/crm/v3/objects/tickets?hapikey=${process.env.HUBSPOT_API_KEY}`, data, {
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
+		// prepare data for POST request
+		const data = JSON.stringify({
+			"properties": {
+				"content": ticketContent,
+				"hs_pipeline": "0",
+				"hs_pipeline_stage": "1",
+				"hs_ticket_priority": "MEDIUM",
+				"hubspot_owner_id": process.env.TICKET_OWNER_ID.toString(),
+				"subject": ticketSubject,
+			},
+		});
 
-	return {
-		statusCode: 200,
-		//body: postResponse.data,
-	};
+		try {
+			// create ticket
+			const postResponse = await axios.post(`https://api.hubapi.com/crm/v3/objects/tickets?hapikey=${process.env.HUBSPOT_API_KEY}`, data, {
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+			return {
+				statusCode: 201,
+				body: postResponse.data,
+			};
+		} catch (error) {
+			console.error("Ticket creation has failed", error);
+			return {
+				statusCode: 400,
+				message: "Ticket creation has failed",
+				body: error,
+			};
+		}
+	} catch (error) {
+		console.error("Connection to Hubspot API has failed", error);
+		return {
+			statusCode: 400,
+			message: "Connection to Hubspot API has failed",
+			body: error,
+		};
+	}
 };
